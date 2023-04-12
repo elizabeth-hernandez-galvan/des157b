b33489be9484e968575b91f21f7ca808e9011eaf
Elizabeth Hernandez Galvan <32213372+elizabeth-hernandez-galvan@users.noreply.github.com>
2023-04-12 08:22:14 -0700

    completed rainy design

--- script.js
+++ script.js
@@ -5,6 +5,8 @@
     const body = document.querySelector('body');
     // const heading = document.querySelectorAll('h3');
     const banner = document.querySelector('#banner');
+    const rain = document.querySelector('.rain');
+    const sun = document.querySelector('.sun');
     // const text = document.querySelectorAll('.text');
     const sections = document.querySelectorAll('section');
     let mode = 'winter';
@@ -14,7 +16,7 @@
             body.className = 'switch';
             banner.className = 'switch';
             button.className = 'switch';
-            
+            rain.classList.replace('rain' , 'sun');
             // heading.className = 'switch';
             for (const section of sections) {
                 section.className = 'switch';
@@ -24,6 +26,7 @@
             body.removeAttribute('class');
             banner.removeAttribute('class');
             button.removeAttribute('class');
+            rain.classList.replace('sun' , 'rain');
             for (const section of sections) {
                 section.removeAttribute('class');
             }
