d06e35f4eac8ee561574d7613b7fb421dcfaeb33
Elizabeth Hernandez Galvan <32213372+elizabeth-hernandez-galvan@users.noreply.github.com>
2023-04-18 16:26:56 -0700

    added video outline

---
+++ studio1/script.js
@@ -0,0 +1,43 @@
+(function() {
+    'use strict';
+
+    const myVideo = document.querySelector('#myVideo');
+    const fs = document.querySelector('.fa-expand');
+
+    const line1 = document.querySelector('#line1');
+    const line2 = document.querySelector('#line2');
+    const line3 = document.querySelector('#line3');
+    const line4 = document.querySelector('#line4');
+    const line5 = document.querySelector('#line5');
+
+    const quote = {
+        start: [0,5,8],
+        stop:[4,7,10],
+        line: [line1, line2, line3, line4, line5]
+    }
+
+    const intervalID = setInterval(checkTime, 1000)
+    
+    function checkTime(){
+        console.log(parseInt(myVideo.currentTime));
+
+        for(let i = 0; i < quote.start.length; i++) {
+            if (quote.start[i] < myVideo.currentTime && myVideo.currentTime < quote.stop[i] ) {
+                quote.line[i].className = "showing";
+            } else {
+                quote.line[i].className = "hidden";
+            }
+        }
+    }
+
+    fs.addEventListener('click', function() {
+        // The fullscreenElement attribute returns null if the element is in windowed mode
+        if (!document.fullscreenElement) {
+            // document.documentElement returns the Element that is a direct child of the document, the <html> element
+            document.documentElement.requestFullscreen();
+        } else {
+            document.exitFullscreen();
+
+        }
+    });
+})();
