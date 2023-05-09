1441c7a02c172069062e0264c4b86ee925f9b7d7
Elizabeth Hernandez Galvan <32213372+elizabeth-hernandez-galvan@users.noreply.github.com>
2023-05-09 12:26:43 -0700

    completed friends list section, errors with saving data

---
+++ friendslist/part2/script.js
@@ -0,0 +1,41 @@
+// JS here
+Parse.initialize("nil0o4Lf31kRWYCZ7sryA0phw6fbaYhvYd2zndsb","U2c69oEFziyEWAKLY0YTbZ4lQw4wsSfAo7IIeqZO"); 
+Parse.serverURL = 'https://parseapi.back4app.com/'
+
+async function displayFriends(){
+    const friends = Parse.Object.extend('Friends');
+    const query = new Parse.Query(friends);
+    const results = await query.ascending('lname').find();
+    console.log(results);
+}
+
+displayFriends();
+
+const newBtn = document.getElementById("newbtn");
+const editBtns = document.querySelectorAll(".fa-edit");
+const addFriendForm = document.getElementById("add-friend");
+const editFriendForm = document.getElementById("edit-friend");
+
+
+newBtn.addEventListener("click", function(event){
+    event.preventDefault();
+    addFriendForm.className = "add-friend-onscreen";
+})
+
+addFriendForm.addEventListener("submit", function(event){
+    event.preventDefault();
+    addFriendForm.className = "add-friend-offscreen";
+})
+
+
+for(let i=0; i<editBtns.length; i++){
+    editBtns[i].addEventListener("click", function(event){
+        event.preventDefault();
+        editFriendForm.className = "edit-friend-onscreen";
+    })
+}
+
+editFriendForm.addEventListener("submit", function(event){
+    event.preventDefault();
+    editFriendForm.className = "edit-friend-offscreen";
+})
--- friendslist/part3/script.js
+++ friendslist/part3/script.js
@@ -7,6 +7,36 @@ async function displayFriends(){
     const query = new Parse.Query(friends);
     const results = await query.ascending('lname').find();
     console.log(results);
+
+    results.forEach(function(eachFriend) {
+        const id = eachFriend.id;
+        const lname = eachFriend.get('lname');
+        const fname = eachFriend.get('fname');
+        const email = eachFriend.get('email');
+        const facebook = eachFriend.get('facebook');
+        const twitter = eachFriend.get('twitter');
+        const instagram = eachFriend.get('instagram');
+        const linkedin = eachFriend.get('linkedin');
+
+        const theListItem = document.createElement("li");
+        theListItem.setAttribute("id", `r-${id}`);
+        theListItem.innerHTML = `
+        <div class="name">${fname} ${lname} </div>
+        <div class="email">
+            <i class="fas fa-envelope-square"></i> ${email}
+        </div>
+        <div class="social">
+            <a href="${facebook}"><i class="fab fa-facebook-square"></i></a>
+            <a href="${twitter}"><i class="fab fa-twitter-square"></i></a>
+            <a href="${instagram}"><i class="fab fa-instagram"></i></a>
+            <a href="${linkedin}"><i class="fab fa-linkedin"></i></a>
+        </div>
+        <i class="fas fa-edit" id="e-${id}"></i>
+        <i class="fas fa-times-circle" id="d-${id}"></i>`;
+
+        friendList.append(theListItem);
+    });
+
 }
 
 displayFriends();
@@ -15,6 +45,7 @@ const newBtn = document.getElementById("newbtn");
 const editBtns = document.querySelectorAll(".fa-edit");
 const addFriendForm = document.getElementById("add-friend");
 const editFriendForm = document.getElementById("edit-friend");
+const friendList = document.querySelector("main ol");
 
 
 newBtn.addEventListener("click", function(event){
