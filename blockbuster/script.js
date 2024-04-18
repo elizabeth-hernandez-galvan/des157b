13f03a8ec8eac1024078e9683ccea9b1cfe9454d
Elizabeth Hernandez Galvan <32213372+elizabeth-hernandez-galvan@users.noreply.github.com>
2024-04-17 21:31:37 -0700

    blockbuster responsive

--- blockbuster/script.js
+++ blockbuster/script.js
@@ -2,14 +2,17 @@
     'use strict';
 
     const fs = document.querySelector('.fa-expand');
-    const section = document.querySelector('#section');
+    const mute = document.querySelector('#mute');
+    
     const line1 = document.querySelector('#line1');
     const line2 = document.querySelector('#line2');
     const line3 = document.querySelector('#line3');
 
     const intervalID = setInterval(checkTime, 1000);
+    const intervalvert = setInterval(checkTimeV, 1000);
 
     const video = document.getElementById('video');
+    const videov = document.getElementById('video-vertical');
 
     fs.addEventListener('click', function(){
 
@@ -20,6 +23,12 @@
         }
     });
 
+    // Background Music
+    mute.addEventListener('click', musicPlay);
+    function musicPlay() {
+        videov.play();
+        document.removeEventListener('click', musicPlay);
+    }
 
 
     function checkTime() {
@@ -34,6 +43,24 @@
             line2.className = "hidden";
         }
         if (12 < video.currentTime && video.currentTime < 16) {
+            line3.className = "showing";
+        } else {
+            line3.className = "hidden";
+        }
+    }    
+
+    function checkTimeV() {
+        if (1 < videov.currentTime && videov.currentTime < 4){
+            line1.className = "showing";
+        } else {
+            line1.className = "hidden";
+        }
+        if (8 < videov.currentTime && videov.currentTime < 11) {
+            line2.className = "showing";
+        } else {
+            line2.className = "hidden";
+        }
+        if (12 < videov.currentTime && videov.currentTime < 16) {
             line3.className = "showing";
         } else {
             line3.className = "hidden";
