3120dc5a7928df48917d4b7e849970fe2602cb83
Elizabeth Hernandez Galvan <32213372+elizabeth-hernandez-galvan@users.noreply.github.com>
2024-05-15 19:55:08 -0700

    adding interact library

--- ministudio3/script.js
+++ ministudio3/script.js
@@ -11,3 +11,18 @@ var granimInstance = new Granim({
         }
     }
  });
+
+ interact('.tap-target')
+  .on('tap', function (event) {
+    event.currentTarget.classList.toggle('switch-bg')
+    event.preventDefault()
+  })
+  .on('doubletap', function (event) {
+    event.currentTarget.classList.toggle('large')
+    event.currentTarget.classList.remove('rotate')
+    event.preventDefault()
+  })
+  .on('hold', function (event) {
+    event.currentTarget.classList.toggle('rotate')
+    event.currentTarget.classList.remove('large')
+  })
