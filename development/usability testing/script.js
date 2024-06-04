8f39426d318ec1bad64aad23744e91f4655931e1
Elizabeth Hernandez Galvan <32213372+elizabeth-hernandez-galvan@users.noreply.github.com>
2024-06-04 07:38:20 -0700

    user testing

---
+++ development/usability testing/script.js
@@ -0,0 +1,41 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const popupOverlay = document.getElementById('popupOverlay');
+    const ats = document.getElementById('ats');
+    const closePopup = document.getElementById('closePopup');
+
+    // Function to open the popup
+    function openPopup() {
+        popupOverlay.style.display = 'block';
+    }
+
+    // Function to close the popup
+    function closePopupFunc() {
+        popupOverlay.style.display = 'none';
+    }
+
+    // Event listeners
+    openPopup();
+
+    // Close the popup when the close button is clicked
+    closePopup.addEventListener('click', closePopupFunc);
+
+    // Close the popup when clicking outside the popup content
+    popupOverlay.addEventListener('click', function (event) {
+        if (event.target === popupOverlay) {
+            closePopupFunc();
+        }
+    });
+});
+
+
+$( function() {
+	$( ".draggable" ).draggable();
+	$( "#droppable" ).droppable({
+	  drop: function( event, ui ) {
+		$( this )
+		  .addClass( "ui-state-highlight" )
+		  .find( "p" )
+			.html( "Dropped!" );
+	  }
+	});
+  } );
