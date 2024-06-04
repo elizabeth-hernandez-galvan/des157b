ade6180b4a91d9ab9791f81f58ca551a8c0be999
Elizabeth Hernandez Galvan <32213372+elizabeth-hernandez-galvan@users.noreply.github.com>
2024-06-03 22:37:04 -0700

    jQuery UI Drag & Drop works

--- development/version2/script.js
+++ development/version2/script.js
@@ -1,56 +1,12 @@
-$(document).ready(function(){
-	$('#reset').on('click', function(){
-		$('ul').attr('id', 'draggable');
-		$('.p_lang').removeAttr('disabled', 'disabled');
-		$('.p_lang').val('');
-		$('#result_text').text('');
+
+$( function() {
+	$( ".draggable" ).draggable();
+	$( "#droppable" ).droppable({
+	  drop: function( event, ui ) {
+		$( this )
+		  .addClass( "ui-state-highlight" )
+		  .find( "p" )
+			.html( "Dropped!" );
+	  }
 	});
-	  
-		$('#submit').on('click', function(){
-			var text = $('#word').val();
-		if(text != ""){
-			$('#result_text').text(text);
-		}
-		else{
-			alert("Please select your choice first");
-		}
-	});
-	  
-		$('li').mouseover(function(){
-			$(this).css('cursor', 'pointer');
-		});
-		$( "#draggable li" ).draggable({helper: 'clone'});
-		$(".p_lang").droppable({
-			accept: "#draggable li",
-			drop: function(ev, ui) {
-			$(this).insertAtCaret(ui.draggable.text());
-			$(this).attr('disabled', 'disabled');
-			$("#draggable").removeAttr('id');
-			}
-		});
-});
-  
-	$.fn.insertAtCaret = function (myValue) {
-	return this.each(function(){
-	if (document.selection) {
-		this.focus();
-		sel = document.selection.createRange();
-		sel.text = myValue;
-		this.focus();
-	}
-  
-	else if (this.selectionStart || this.selectionStart == '0') {
-		var startPos = this.selectionStart;
-		var endPos = this.selectionEnd;
-		var scrollTop = this.scrollTop;
-		this.value = this.value.substring(0, startPos)+ myValue+ this.value.substring(endPos,this.value.length);
-		this.focus();
-		this.selectionStart = startPos + myValue.length;
-		this.selectionEnd = startPos + myValue.length;
-		this.scrollTop = scrollTop;
-	} else {
-		this.value += myValue;
-		this.focus();
-	}
-	});
-	};
+  } );
